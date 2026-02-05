2eba999b9e910d7cccd1b669b82e378ef06cc936
Vasanth2109 <donappu2004@gmail.com>
2026-02-05 20:34:32 +0530

    Transpose of an Array

---
+++ Arrays/MatrixTranspose.java
@@ -0,0 +1,20 @@
+public class MatrixTranspose
+{
+public static void main ( String args[])
+{
+int[][] array={
+               {1,2,3},
+               {4,5,6},
+               {7,8,9}
+              };
+for(int i=0;i<array[0].length;i++)
+{
+for(int j=0;j<array.length;j++)
+{
+System.out.print(array[j][i] + " ");
+}
+System.out.println();
+}
+}
+}
+
