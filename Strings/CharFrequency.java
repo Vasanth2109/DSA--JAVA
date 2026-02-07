bf916f169820833d5656401382f78af5a2c3bba0
Vasanth2109 <donappu2004@gmail.com>
2026-02-07 21:01:12 +0530

    Count how many times each character appears in String

---
+++ Strings/CharFrequency.java
@@ -0,0 +1,22 @@
+public class CharFrequency
+{
+public static void main(String[] args)
+{
+String s = "banana";
+int[] freq = new int[256];
+for (int i = 0; i < s.length(); i++)
+{
+char ch = s.charAt(i);
+freq[ch]++;
+}
+for (int i = 0; i < s.length(); i++)
+{
+char ch = s.charAt(i);
+if (freq[ch] != 0)
+{
+System.out.println(ch + " : " + freq[ch]);
+freq[ch] = 0; // avoid duplicate printing
+}
+}
+}
+}
