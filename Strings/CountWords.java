55d253e5d8d903acffe031d82d0d7318e5d537c1
Vasanth2109 <donappu2004@gmail.com>
2026-02-08 07:07:16 +0530

    Counts the words in a Sentence

---
+++ Strings/CountWords.java
@@ -0,0 +1,9 @@
+public class CountWords
+{
+public static void main(String[] args)
+{
+String s = "welcome to java programming";
+String[] words = s.split(" ");
+System.out.println("Word count = " + words.length);
+}
+}
